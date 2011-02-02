e9b200c003f7577082b24daca72b99d3dce655c1
Erik Krasner-Karpen <erik.krasnerkarpen@gmail.com>
2011-02-01 23:40:33 -0300

    wrote parser that works for Phaedrus

---
+++ plato_parser.py
@@ -0,0 +1,17 @@
+def parse_standard_dialogue(file_name):
+    dialogue = open(file_name,'r')
+    for line in dialogue:
+        if '-----' in line:
+            break
+    dialogue.next()
+    dialogue_string = ""
+    dialogue_list = []
+    for line in dialogue:
+        if line != '\n':
+            if dialogue_string:
+                dialogue_string += ' '
+            dialogue_string += line[:-1]
+        else:
+            dialogue_list.append(dialogue_string)
+            dialogue_string = ""
+    return dialogue_list
