4658a29f5f3659246fc4d6cf3f83917fccec4b9b
Erik Krasner-Karpen <erik.krasnerkarpen@gmail.com>
2011-02-02 20:03:55 -0300

    more parsing methods

--- plato_parser.py
+++ plato_parser.py
@@ -1,3 +1,24 @@
+def is_prefix(str1,str2):
+    return len(str2) >= len(str1) and str2[:len(str1)] == str1
+
+def find_speaker(speaker_list,speaker_prefix):
+    matches = filter(lambda speaker:is_prefix(speaker_prefix,speaker),speaker_list)
+    if len(matches) == 0:
+        return None
+    return matches[0]
+
+def get_speakers(file_object):
+    speakers = []
+    parsing_speakers = False
+    for line in file_object:
+        if not parsing_speakers and line == "Persons of the Dialogue\n":
+            parsing_speakers = True
+        elif parsing_speakers:
+            if "-----" in line:
+                break
+            speakers.append(line[:line.index(",")] if "," in line else line[:-1])
+    return speakers
+
 def parse_standard_dialogue(file_name):
     dialogue = open(file_name,'r')
     for line in dialogue:
