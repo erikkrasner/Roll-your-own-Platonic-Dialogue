6d085b4c4fcfdcf84f5c41c3ea7cc785be08e225
Erik Krasner-Karpen <erik.krasnerkarpen@gmail.com>
2011-02-08 20:42:44 -0300

    wrote out the logic for the PlatoWriter to write out a Platonic dialogue

--- plato_writer.py
+++ plato_writer.py
@@ -2,7 +2,24 @@ import pickle
 from markov import *
 
 class PlatoWriter:
-    def filter_corpus(corpus, character_list):
+    def __init__(self, character_list):
+        sequence_corpus = pickle.load(open('datafiles/sequence.dat','r'))
+        sequence_corpus = self.filter_corpus(sequence_corpus, character_list)
+        self.sequence_writer = MarkovWriter(sequence_corpus)
+        self.speaker_writers = {}
+        for speaker in character_list:
+            if speaker == 'SOCRATES':
+                #Socrates needs to be in two files to overcome 10MB Google App Engine limit
+                soc_string = open('datafiles/SOCRATES1.dat','r').read() + open('datafiles/SOCRATES2.dat','r').read()
+                speaker_corpus = pickle.loads(soc_string)
+            else:
+                speaker_corpus = pickle.load(open("datafiles/%s.dat" % speaker, 'r'))
+            speaker_writers[speaker] = MarkovWriter(speaker_corpus)
+    def write(self, order):
+        sequence_string = self.sequence_writer.write(3)
+        seqeunce = sequece_string.split()
+        return map(lambda speaker: speaker_writers[speaker].write(order),sequence)
+    def filter_corpus(self, corpus, character_list):
         def subset(seq1,seq2):
             for elem in seq1:
                 if elem not in seq2:
@@ -23,3 +40,4 @@ class PlatoWriter:
             new_corpus.append(new_row)
             new_corpus.order = corpus.order
         return new_corpus
+    
