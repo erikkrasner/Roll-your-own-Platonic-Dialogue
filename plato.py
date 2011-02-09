397b2e56f7e747feb6e9523cb21f50803c2b9ee6
Erik Krasner-Karpen <erik.krasnerkarpen@gmail.com>
2011-02-09 14:08:30 -0300

    progress on form handling

--- plato.py
+++ plato.py
@@ -1,20 +1,35 @@
 import os
 import cgi
 import wsgiref.handlers
+from google.appengine.ext import db
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import template
 
 import plato_writer
 
+class Dialogue(db.Model):
+    id = db.StringProperty()
+    characters = db.StringProperty()
+    text = db.StringProperty(multiline=True)
+
 class MainPage(webapp.RequestHandler):
     def get(self):
+        id = self.request.get('id')
         characters = open('datafiles/characters.txt','r').read().split(',')
-        template_values = {'characters':characters}
+        template_values = {'id':id,'characters':characters}
         html = template.render('plato.html', template_values)
         self.response.out.write(html)
 
+class ResultGenerator(webapp.RequestHandler):
+    def getID(self):
+        return 1
+    def post(self):
+        id = self.getID()
+        self.redirect("/?id=%d" % id)
+
 application = webapp.WSGIApplication([
-        ('/',MainPage)
+        ('/',MainPage),
+        ('/result',ResultGenerator)
 ],debug=True)
 
 def main():
