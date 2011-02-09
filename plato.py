4d55ded886c4073abfed8df1300c0771b1b91cda
Erik Krasner-Karpen <erik.krasnerkarpen@gmail.com>
2011-02-09 18:16:11 -0300

    front end

--- plato.py
+++ plato.py
@@ -18,6 +18,7 @@ class ID(db.Model):
 class MainPage(webapp.RequestHandler):
     def get(self):
         id = self.request.get('id')
+        dialogue = db.GqlQuery("SELECT * FROM Dialogue WHERE id=:1", id)
         characters = open('datafiles/characters.txt','r').read().split(',')
         template_values = {'id':id,'characters':characters}
         html = template.render('plato.html', template_values)
@@ -36,11 +37,21 @@ class ResultGenerator(webapp.RequestHandler):
         db.put(id)
         return value
     def post(self):
+        def structure_dialogue(speech_pair):
+            speaker, speech = speech_pair
+            return "<p><b>{0}:</b>{1}</p>".format(speaker,speech)
         new_dialogue = Dialogue()
         id = self.getID()
+        title = self.request.get("title")
         characters = self.request.get_all("character")
         order = self.request.get("order")
-        text= PlatoWriter(characters).write(order)
+        text_list= PlatoWriter(characters).write(int(order))
+        text = "<h2>%s</h2><br>By Plato, trans. Benjamin Jowett with help from Mark V Shaney<br><br>" % title.title()
+        text += "Persons of the Dialogue<br>"
+        text += reduce(lambda a,b: a + "<br>" + b, map(lambda x: x.upper(),characters))
+        text += "<br>-------------<br>"
+        text += map(structure_dialogue,text_list)
+        text += "<br>THE END"
         new_dialogue.id = str(id)
         new_dialogue.characters = reduce(lambda a,b: a+" "+b, characters)
         new_dialogue.order = order
